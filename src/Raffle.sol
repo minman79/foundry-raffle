c70a8351d7ed90594cbcdcd93953c79541a9938a
minman79 <minman79@protonmail.com>
2024-09-12 11:54:04 +0100

    Added automation to pick winner after time interval has passeed via chainlink using the checkUpkeep function and re-factored the pickWinner function to performUpkeep when bool return true

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -97,12 +97,35 @@ contract Raffle is VRFConsumerBaseV2Plus {
         emit RaffleEntered(msg.sender);
     }
 
-    // 1. Get a random number
-    // 2. Use random number to pick a player
-    // 3. Makes front end "indexing" easier
-    function pickWinner() external {
+    // This checks when is it ready to pick a winner
+    /**
+     * @dev This is the function that the Chainlink nodes will call to see if the lottery is ready to have a winner picked.
+     * The following should be true to have a winner picked.
+     * 1. The time interval has passed between raffle runs
+     * 2. The lottery is open 
+     * 3. The contract has ETH (Does it have players)
+     * 4. Implicitly, your subscription has LINK
+     * @param - ignored 
+     * @return upkeepNeeded - true if it's time to restart the lottery 
+     * @return - ignored
+     */
+    function checkUpkeep(bytes memory /* checkData */) public view returns (bool upkeepNeeded, bytes memory /* preformData */) {  // upkeepNeeded is initalized in this form and defaults to false
+        bool timeHasPassed = ((block.timestamp - s_lastTimeStamp) >= i_interval);
+        bool isOpen = s_raffleState == RaffleState.OPEN;
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = s_players.length > 0;
+        upkeepNeeded = timeHasPassed && isOpen && hasBalance && hasPlayers;
+        return(upkeepNeeded, "");
+        }
+    }
+
+    // 1. Get a random number  -- Chainlink VRF
+    // 2. Use random number to pick a player -- fulfillRandomWords()
+    // 3. Be automatically called -- Chainlink Automation
+    function performUpkeep(bytes calldata /* performData */) external {
         // has enough time passed using block.timestamp till we can pick a winner
-        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
             revert();
         }
 
